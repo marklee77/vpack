0795ed6c065d48a27e81df3e3947c4816f90d5c9
Mark Stillwell <marklee@fortawesome.org>
2014-01-30 16:20:17 +0000

    added interface to vpsolver, much faster than openopt

--- vectorpack/interface.py
+++ vectorpack/interface.py
@@ -218,6 +218,59 @@ def pack_vectors_openopt(**kwargs):
         'runtime' : stop_time - start_time,
     }
 
+def pack_vectors_vpsolver(**kwargs):
+    from pyvpsolver import solve_mvbp
+
+    solver = kwargs.get('solver', 'glpk')
+    verbose = kwargs.get('verbose', False)
+
+    problem = kwargs.get('problem', None)
+
+    items = problem.get('items', None)
+    bins = problem.get('bins', None)
+
+    # FIXME: smarter handling of items of the same type...
+    # brandao can only handle int contents...
+    vpsolver_items = [[[1000 * int(r) for r in item]] for item in items]
+
+    bin_set = set(tuple(bin_) for bin_ in bins)
+    if len(bin_set) != 1:
+        # FIXME: well, for now...
+        raise Exception('vpsolver can only work with homogeneous bins')
+
+    vpsolver_bins = [tuple(1000 * int(c) for c in bins[0])]
+
+    b = [1] * len(items)
+    cost = [1]
+
+    script = "vpsolver_" + solver + ".sh"
+
+    # FIXME: single for balance?
+    start_time = time.process_time()
+    _, sol = solve_mvbp(vpsolver_bins, vpsolver_items, b, cost, 
+                        verbose=verbose, script=script)
+    stop_time = time.process_time()
+
+    mapping = [None] * len(items)
+    for b, (_, bin_contents) in enumerate(sol[0]):
+        for i, _ in bin_contents:
+            mapping[i] = b
+
+    # FIXME: method names?
+    return {
+        'solver-githash' : '__GITHASH__',
+        'problem-argshash' : problem.get('argshash', None),
+        'algorithm' : { 'type' : 'exact', 
+                        'family' : 'brandao2013vsv', 
+                        'solver' : solver,
+                        'verbose' : verbose },
+        'datetime' : datetime.now(),
+        'mapping' : mapping,
+        'failcount' : mapping.count(None),
+        'bincount' : len(Counter(mapping)),
+        'verified' : verify_mapping(items=items, bins=bins, mapping=mapping),
+        'runtime' : stop_time - start_time,
+    }
 
 def main(argv=None):
 
